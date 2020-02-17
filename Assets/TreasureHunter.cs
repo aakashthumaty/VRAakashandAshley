e5d00b3049b0d972468bfda5df80fbbdb70536f4
Neehar Thumaty <Neehar@aakashs-mbp.wireless-guest.unc.edu>
2020-02-17 11:08:02 -0500

    forcegrab halfway working

--- Assets/TreasureHunter.cs
+++ Assets/TreasureHunter.cs
@@ -4,10 +4,15 @@
 
 //this dude rocks: https://www.youtube.com/watch?v=_yf5vzZ2sYE
 
+//we referenced lots of nicks code for the grabbing/GO children/raycasting with the oculus controllers
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+
+public enum AttachmentRule{KeepRelative,KeepWorld,SnapToTarget}
+
 
 public class TreasureHunter : MonoBehaviour
 {
@@ -19,6 +24,8 @@ public class TreasureHunter : MonoBehaviour
 
     public TextMesh score;
 
+    CollectibleTreasure thingIGrabbed;
+
 //selectable ness
 
     [SerializeField] private string selectableTag = "Selectable";
@@ -26,6 +33,9 @@ public class TreasureHunter : MonoBehaviour
     [SerializeField] private Material defaultMaterial;
 
     private Transform currentSelected;
+
+    public GameObject leftPointerObject;
+    public GameObject rightPointerObject;
 
 
 
@@ -38,27 +48,113 @@ public class TreasureHunter : MonoBehaviour
 
     }
 
+    public void attachGameObjectToAChildGameObject(GameObject GOToAttach, GameObject newParent, AttachmentRule locationRule, AttachmentRule rotationRule, AttachmentRule scaleRule, bool weld){
+        GOToAttach.transform.parent=newParent.transform;
+        handleAttachmentRules(GOToAttach,locationRule,rotationRule,scaleRule);
+        if (weld){
+            simulatePhysics(GOToAttach,Vector3.zero,false);
+        }
+    }
+
+    public static void detachGameObject(GameObject GOToDetach, AttachmentRule locationRule, AttachmentRule rotationRule, AttachmentRule scaleRule){
+        //making the parent null sets its parent to the world origin (meaning relative & global transforms become the same)
+        GOToDetach.transform.parent=null;
+        handleAttachmentRules(GOToDetach,locationRule,rotationRule,scaleRule);
+    }
+
+        public static void handleAttachmentRules(GameObject GOToHandle, AttachmentRule locationRule, AttachmentRule rotationRule, AttachmentRule scaleRule){
+        GOToHandle.transform.localPosition=
+        (locationRule==AttachmentRule.KeepRelative)?GOToHandle.transform.position:
+        //technically don't need to change anything but I wanted to compress into ternary
+        (locationRule==AttachmentRule.KeepWorld)?GOToHandle.transform.localPosition:
+        new Vector3(0,0,0);
+
+        //localRotation in Unity is actually a Quaternion, so we need to specifically ask for Euler angles
+        GOToHandle.transform.localEulerAngles=
+        (rotationRule==AttachmentRule.KeepRelative)?GOToHandle.transform.eulerAngles:
+        //technically don't need to change anything but I wanted to compress into ternary
+        (rotationRule==AttachmentRule.KeepWorld)?GOToHandle.transform.localEulerAngles:
+        new Vector3(0,0,0);
+
+        GOToHandle.transform.localScale=
+        (scaleRule==AttachmentRule.KeepRelative)?GOToHandle.transform.lossyScale:
+        //technically don't need to change anything but I wanted to compress into ternary
+        (scaleRule==AttachmentRule.KeepWorld)?GOToHandle.transform.localScale:
+        new Vector3(1,1,1);
+    }
+
+    public void simulatePhysics(GameObject target,Vector3 oldParentVelocity,bool simulate){
+        Rigidbody rb=target.GetComponent<Rigidbody>();
+        if (rb){
+            if (!simulate){
+                Destroy(rb);
+            } 
+        } else{
+            if (simulate){
+                //there's actually a problem here relative to the UE4 version since Unity doesn't have this simple "simulate physics" option
+                //The object will NOT preserve momentum when you throw it like in UE4.
+                //need to set its velocity itself.... even if you switch the kinematic/gravity settings around instead of deleting/adding rb
+                Rigidbody newRB=target.AddComponent<Rigidbody>();
+                newRB.velocity=oldParentVelocity;
+            }
+        }
+    }
+
+        void forceGrab(bool pressedA){
+        print("yo you force grabbed");
+        RaycastHit outHit;
+        //notice I'm using the layer mask again
+        if (Physics.Raycast(rightPointerObject.transform.position, rightPointerObject.transform.forward, out outHit, 100.0f))//,collectiblesMask))
+        {
+            AttachmentRule howToAttach=pressedA?AttachmentRule.KeepWorld:AttachmentRule.SnapToTarget;
+            attachGameObjectToAChildGameObject(outHit.collider.gameObject,rightPointerObject.gameObject,howToAttach,howToAttach,AttachmentRule.KeepWorld,true);
+            thingIGrabbed=outHit.collider.gameObject.GetComponent<CollectibleTreasure>();
+        }
+    }
+
+    // void letGo(){
+    //     if (thingIGrabbed){
+    //         Collider[] overlappingThingsWithLeftHand=Physics.OverlapSphere(leftPointerObject.transform.position,0.01f,collectiblesMask);
+    //         if (overlappingThingsWithLeftHand.Length>0){
+    //             if (thingOnGun){
+    //                 detachGameObject(thingOnGun,AttachmentRule.KeepWorld,AttachmentRule.KeepWorld,AttachmentRule.KeepWorld);
+    //                 simulatePhysics(thingOnGun,Vector3.zero,true);
+    //             }
+    //             attachGameObjectToAChildGameObject(overlappingThingsWithLeftHand[0].gameObject,leftPointerObject,AttachmentRule.SnapToTarget,AttachmentRule.SnapToTarget,AttachmentRule.KeepWorld,true);
+    //             thingOnGun=overlappingThingsWithLeftHand[0].gameObject;
+    //             thingIGrabbed=null;
+    //         }else{
+    //             detachGameObject(thingIGrabbed.gameObject,AttachmentRule.KeepWorld,AttachmentRule.KeepWorld,AttachmentRule.KeepWorld);
+    //             simulatePhysics(thingIGrabbed.gameObject,(rightPointerObject.gameObject.transform.position-previousPointerPos)/Time.deltaTime,true);
+    //             thingIGrabbed=null;
+    //         }
+    //     }
+    // }
+
     // Update is called once per frame
     void Update()
     {
-
-    /////selectableness
-
-        if (currentSelected != null)
+        //var selection;
+       if (currentSelected != null)
         {
             var selectionRenderer = currentSelected.GetComponent<Renderer>();
             selectionRenderer.material = defaultMaterial;
             currentSelected = null;
         }
-        
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {
-
-            var selection = hit.transform;
+        ///oculus raycasting
+        if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger) || Input.anyKey){
+            //var ray = 
+            RaycastHit outHit;
+            if (Physics.Raycast(rightPointerObject.transform.position, rightPointerObject.transform.forward, out outHit, 100.0f))
+            {
+                win.fontSize = 20;
+                win.font.material.color = Color.blue;
+                win.text = "Hi Ashley" + outHit.transform.gameObject.name;
+                print("RAY RAY RAY!");
+                var selection = outHit.transform;
             if (selection.CompareTag(selectableTag))
             {
+                forceGrab(false);
                 foreach (char c in Input.inputString){
                     
                     if(c == 'g'){
@@ -69,8 +165,8 @@ public class TreasureHunter : MonoBehaviour
                         var n = valiii.pf;
 
                         int count;
-                        invent.dict.TryGetValue((CollectibleTreasure)AssetDatabase.LoadAssetAtPath("Assets/" + n, typeof(CollectibleTreasure)), out count);
-                        invent.dict[(CollectibleTreasure)AssetDatabase.LoadAssetAtPath("Assets/" + n, typeof(CollectibleTreasure))] = count + 1;
+                        invent.dict.TryGetValue((CollectibleTreasure)Resources.Load("Assets/" + n, typeof(CollectibleTreasure)), out count);
+                        invent.dict[(CollectibleTreasure)Resources.Load("Assets/" + n, typeof(CollectibleTreasure))] = count + 1;
 
                         Destroy(selection.gameObject);
                     }
@@ -87,7 +183,50 @@ public class TreasureHunter : MonoBehaviour
 
                 currentSelected = selection;
             }
+            }
         }
+
+    /////selectableness
+
+        
+        // var rayforkb = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // RaycastHit hit;
+        // if (Physics.Raycast(rayforkb, out hit))
+        // {
+
+        //     //var 
+        //     var selection = hit.transform;
+        //     if (selection.CompareTag(selectableTag))
+        //     {
+        //         foreach (char c in Input.inputString){
+                    
+        //             if(c == 'g'){
+        //                 print("OMG SELECTED!!!");
+
+        //                 var valiii = selection.gameObject.GetComponent<CollectibleTreasure>();
+        //                 print(valiii.value);
+        //                 var n = valiii.pf;
+
+        //                 int count;
+        //                 invent.dict.TryGetValue((CollectibleTreasure)Resources.Load(n, typeof(CollectibleTreasure)), out count);
+        //                 invent.dict[(CollectibleTreasure)Resources.Load(n, typeof(CollectibleTreasure))] = count + 1;
+
+        //                 Destroy(selection.gameObject);
+        //             }
+
+        //         }
+
+                
+        //         var selectionRenderer = selection.GetComponent<Renderer>();
+
+        //         if (selectionRenderer != null)
+        //         {
+        //             selectionRenderer.material = highlightMaterial;
+        //         }
+
+        //         currentSelected = selection;
+        //     }
+        // }
 
     /////selectableness
 
